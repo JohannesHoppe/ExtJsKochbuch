43570c1178384a3164aa1c5bf26bb058c940470a
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-03-01 01:14:38 +0100

    figure + figcaption for images - inserted via Ext.js

--- Kochbuch/included/extra.js
+++ Kochbuch/included/extra.js
@@ -1,10 +1,29 @@
 var setTargets = function() {
-    var link, i = 0;
-        while (link = document.links[i++]) {
-            if (link.href.indexOf('http://') !== -1) {
+    var link, i = 0;   
+    while (link = document.links[i++]) {
+        if (link.href.indexOf('http://') !== -1) {
             link.target = '_blank';
         }
     }
 }
 
+var addCaption = function() {
+       
+    Ext.select('img').each(function(value) {
+
+        var text = value.getAttribute('alt');
+        
+        if (text) {
+        
+        value.wrap({
+                tag: 'figure',
+            }).parent().createChild({
+                tag: 'figcaption',
+                html: text
+            });
+        }        
+    });
+}
+
 Ext.onReady(setTargets);
+Ext.onReady(addCaption);
