b0bb8c753a75d35143ea71345bf97394b32363be
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-03-02 18:19:32 +0100

    adding a really, really empty ASP.NET MVC 5 project

---
+++ examples/Kochbuch/Kochbuch/App_Start/BundleConfig.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Optimization;
+
+namespace Kochbuch.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/scripts"));
+
+            bundles.Add(new StyleBundle("~/Content/styles"));
+        }
+    }
+}
---
+++ examples/Kochbuch/Kochbuch/Global.asax.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Kochbuch.App_Start;
+
+namespace Kochbuch
+{
+    public class MvcApplication : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
