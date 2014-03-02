0317dbd638721b4f775a6f69dfd575b7f369244a
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-03-02 15:44:13 +0100

    target replacement: check against link.getAttribute('href') (unaltered) instead of link.href (which gets an absolute URL)

--- Kochbuch/included/extra.js
+++ Kochbuch/included/extra.js
@@ -1,7 +1,7 @@
 var setTargets = function() {
     var link, i = 0;   
     while (link = document.links[i++]) {
-        if (link.href.indexOf('http://') !== -1) {
+        if (link.getAttribute('href').indexOf('http://') !== -1) {
             link.target = '_blank';
         }
     }
