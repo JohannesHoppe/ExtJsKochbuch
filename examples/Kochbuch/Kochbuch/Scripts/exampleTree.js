04f4116ea37ef758d097a3ac16d8da6392afb52c
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-03-12 23:09:48 +0100

    hashbang works

--- examples/Kochbuch/Kochbuch/Scripts/exampleTree.js
+++ examples/Kochbuch/Kochbuch/Scripts/exampleTree.js
@@ -1,6 +1,6 @@
 ﻿var exampleTree = (function (Ext, tabManager, hash) {
       
-    var onTreeItemClick = function (record, e) {
+    var onItemClick = function (view, record, item, index, e) {
         if (record.isLeaf()) {
 
             e.stopEvent();
@@ -21,21 +21,54 @@
     };
 
     // enables navigation via keyboard
-    var onTreeAfterRender = function (tree) {
-        var sm = tree.getSelectionModel();
+    var onAfterRender = function (tree) {
+        var selectionModel = tree.getSelectionModel();
 
         Ext.create('Ext.util.KeyNav', tree.view.el, {
             enter: function (e) {
-                if (sm.hasSelection()) {
-                    onTreeItemClick(sm.getSelection()[0], e);
+                if (selectionModel.hasSelection()) {
+                    onItemClick(selectionModel.getSelection()[0], e);
                 }
             }
         });
+
+        tree.expandAll();
+    };
+
+    var firstLoad = true;
+
+    var onLoad = function (store, node, records, successful, eOpts) {
+
+        if (!firstLoad) {
+            return;
+        }
+
+        var exampleId = hash.get("ex");
+        
+        if (exampleId) {
+
+            var record = store.getById(exampleId);
+            if (record) {
+                tabManager.addTab({
+                    url: record.get('href'),
+                    id: record.getId(),
+                    title: record.get('text'),
+                    iconCls: record.get('iconCls')
+                });
+                
+                var selectionModel = store.ownerTree.getSelectionModel();
+                //store.ownerTree.expandNode(record, true);
+                selectionModel.select(record);
+            }
+        }
+
+        firstLoad = false;
     };
 
     return {
-        onTreeAfterRender: onTreeAfterRender,
-        onTreeItemClick: onTreeItemClick
+        onAfterRender: onAfterRender,
+        onItemClick: onItemClick,
+        onLoad: onLoad
     };
 
 })(window.Ext, window.tabManager, window.hash);
