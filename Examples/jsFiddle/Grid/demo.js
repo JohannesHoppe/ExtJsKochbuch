19fec0544b792fecacf505440e41c3c68bcac070
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-03-16 11:40:40 +0100

    still struggling around with the fiddle

--- Examples/jsFiddle/Grid/demo.js
+++ Examples/jsFiddle/Grid/demo.js
@@ -1,7 +1,11 @@
-Ext.application({
-    name: 'Ext JS Fiddle',
-
-    launch: function() {
-        Ext.Msg.alert('Fiddle', 'Welcome to the Ext Js Fiddle!');
-    }
-});
+window.addEvent('domready', function() {
+	new Request.HTML({
+		url: '/gh/get/response.html/zalun/jsFiddleGithubDemo/tree/master/Demo/',
+		data: {'delay': 1},
+		method: 'post',
+		update: 'demo',
+		onSuccess: function(response) {
+			$('demo').highlight();
+		}
+	}).send();
+})
