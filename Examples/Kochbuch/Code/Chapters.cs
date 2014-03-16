9ebd96c02a57f1e9bc156f382384c563a9c2b1ab
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2014-03-16 11:59:30 +0100

    .

--- Examples/Kochbuch/Code/Chapters.cs
+++ Examples/Kochbuch/Code/Chapters.cs
@@ -66,7 +66,7 @@ namespace Kochbuch.Code
             {
                 NodeID = "jsFiddle_grid",
                 Text = "jsFiddle: Grid ",
-                Href = "http://jsfiddle.net/gh/get/extjs/4.2/JohannesHoppe/ExtJsKochbuch/tree/master/Examples/jsFiddle/Grid/",
+                Href = "http://jsfiddle.net/gh/get/extjs/4.2/JohannesHoppe/ExtJsKochbuch/tree/master/Examples/FiddleGrid",
                 Icon = Icon.WeatherCloud,
                 Leaf = true
             };
